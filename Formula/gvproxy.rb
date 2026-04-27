cfd3a479d074c9a55f6779b6799d79a94df2ae93
Sergio López <511389+slp@users.noreply.github.com>
2026-04-27 09:04:30 +0000

    gvproxy: add 0.8.8 bottle.

--- Formula/gvproxy.rb
+++ Formula/gvproxy.rb
@@ -5,6 +5,12 @@ class Gvproxy < Formula
   sha256 "4f7c4885225d71b21f6b547b94d92fc6da4a4fef9d382fdd19c8ea67f67be839"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/slp/homebrew-krun/releases/download/gvproxy-0.8.8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f6e344bbe92bb0ca11d18188fce5b5893693793601b21369b74f5ace737ae7f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbb6775b4d1d3844730c01304e6be74bb9aee9c4e7e6989bebe350e58c144af5"
+  end
+
   depends_on "go" => :build
 
   def install
