884d86ef9b3264b05ee6d8d72caa37c482d79e8b
Sergio Lopez <slp@redhat.com>
2026-09-25 12:15:44 +0200

    libkrun: update to version 1.19.5
    We also need to rename virglrenderer to virglrenderer-krun to avoid a
    conflict with the package from the main Homebrew repository.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,15 +1,9 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/libkrun/libkrun"
-  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.4.tar.gz"
-  sha256 "e8775fab2b460972a67ca6cd936296bb79cdb078d852d712a283cb290dd0b284"
+  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.5.tar.gz"
+  sha256 "f7ffb97b86eb2153c4e7e5f8388c00e971d935e90c52303a8ab43a5a487356e1"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/libkrun/homebrew-krun/releases/download/libkrun-1.19.4"
-    sha256 cellar: :any, arm64_tahoe:   "191425790a550c962508653e9dd1e751fe89b73ae644bbf65081aec3465587e7"
-    sha256 cellar: :any, arm64_sequoia: "20c4064f2adfc35978b827541147d63a47cb25a3065d22b417aab4622b5f5c8b"
-  end
 
   depends_on "lld" => :build
   depends_on "rust" => :build
@@ -18,7 +12,7 @@ class Libkrun < Formula
   depends_on "dtc"
   depends_on "libepoxy"
   depends_on "libkrunfw"
-  depends_on "virglrenderer"
+  depends_on "virglrenderer-krun"
   depends_on "xz"
 
   def install
