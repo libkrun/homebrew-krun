026f57e3d04071ad91ce9a602304a26bf615528d
Sergio López <511389+slp@users.noreply.github.com>
2026-09-25 11:59:48 +0000

    virglrenderer-krun: add 0.10.4e bottle.

--- Formula/virglrenderer-krun.rb
+++ Formula/virglrenderer-krun.rb
@@ -5,6 +5,12 @@ class VirglrendererKrun < Formula
   sha256 "09d000623fbdb966cb604eb48c962a0815e8142383e6066d6494809335b76dbb"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/libkrun/homebrew-krun/releases/download/virglrenderer-krun-0.10.4e"
+    sha256 cellar: :any, arm64_tahoe:   "a1409a29a8054da3bed9ec562a2c03113041d011d146ca2174562679fcfa2243"
+    sha256 cellar: :any, arm64_sequoia: "aa075ef1bbebbeda21663cae2e882cc15469a90a63083ab9612559b5a8fc45da"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
