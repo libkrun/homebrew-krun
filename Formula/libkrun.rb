47a8bdb089dc0239c5153ef17b60fab7d5005f56
Sergio López <511389+slp@users.noreply.github.com>
2026-09-25 11:59:48 +0000

    libkrun: add 1.19.5 bottle.

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -5,6 +5,12 @@ class Libkrun < Formula
   sha256 "f7ffb97b86eb2153c4e7e5f8388c00e971d935e90c52303a8ab43a5a487356e1"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/libkrun/homebrew-krun/releases/download/libkrun-1.19.5"
+    sha256 cellar: :any, arm64_tahoe:   "e6e3eee4efc4a98606b26f2a5571b4a3de01334bbbd6677ea31d4303d4b6214d"
+    sha256 cellar: :any, arm64_sequoia: "48ab7eb539412b56c46fe013b0f2bfda3d6e65e0fef4b9573c6ba312f0d15b72"
+  end
+
   depends_on "lld" => :build
   depends_on "rust" => :build
   # Upstream only supports Hypervisor.framework on arm64
